885ea9b51cdd7270c08673d106dc533049763f0f
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2024-10-09 01:31:50 -0300

    primera estructura

---
+++ src/screens/Guest/ForgotPassword/ForgotPasswordScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./ForgotPasswordScreen.styles";
+
+export function ForgotPasswordScreen() {
+  return (
+    <View>
+      <Text>ForgotPasswordScreen</Text>
+    </View>
+  );
+}
